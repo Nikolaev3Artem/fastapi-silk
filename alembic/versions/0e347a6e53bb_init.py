503065a3e499b814e099c1d48f175a9f4c853752
dhruvgarg.garg123@gmail.com <dhruvgarg.garg123@gmail.com>
2026-02-17 18:43:22 +0000

    feat: setup sqlite and alembic

---
+++ alembic/versions/0e347a6e53bb_init.py
@@ -0,0 +1,28 @@
+"""init
+
+Revision ID: 0e347a6e53bb
+Revises: 
+Create Date: 2026-02-17 18:41:31.012891
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '0e347a6e53bb'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
