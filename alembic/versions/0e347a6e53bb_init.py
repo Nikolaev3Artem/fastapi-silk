4c358760997f72dc6c7e3886ac44c0842407fbf4
dhruvgarg.garg123@gmail.com <dhruvgarg.garg123@gmail.com>
2026-02-17 20:12:05 +0000

    style: fix ruff formatting in alembic files

--- alembic/versions/0e347a6e53bb_init.py
+++ alembic/versions/0e347a6e53bb_init.py
@@ -1,10 +1,11 @@
 """init
 
 Revision ID: 0e347a6e53bb
-Revises: 
+Revises:
 Create Date: 2026-02-17 18:41:31.012891
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -12,7 +13,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '0e347a6e53bb'
+revision: str = "0e347a6e53bb"
 down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
